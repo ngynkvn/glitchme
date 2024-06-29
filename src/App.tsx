346675f04fbfa1b65ed93a65d5481dc6554d8514
Kevin Nguyen <ngynkvn@gmail.com>
2024-06-29 13:44:59 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,110 @@
+import { useMemo, useRef, useState } from 'react'
+import clsx from 'clsx'
+import './App.css'
+import { getContrastMap, getSpans, sortSpansInImage } from './util'
+
+function App() {
+  const [lowThreshold, setLowThreshold] = useState(0.4)
+  const [highThreshold, setHighThreshold] = useState(0.8)
+  const [fileDragged, setFileDragged] = useState(false)
+  const [imgData, setImgData] = useState<ImageData | null>(null)
+  const imgRef = useRef<HTMLCanvasElement | null>(null)
+  const ctx = useMemo(() => imgRef.current?.getContext('2d', { willReadFrequently: true }), [imgRef])
+
+  const showContrast = () => {
+    if (!ctx || !imgData) { return }
+
+    const contrastMap = getContrastMap(imgData, highThreshold, lowThreshold)
+    ctx?.putImageData(contrastMap, 0, 0)
+  }
+
+  const handleLowThresholdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLowThreshold(Number(e.target.value))
+    showContrast()
+  }
+
+
+  const handleHighThresholdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHighThreshold(Number(e.target.value))
+    showContrast()
+  }
+
+  const handleGlitch = () => {
+    if (!ctx || !imgData) { return }
+
+    const contrastMap = getContrastMap(imgData, highThreshold, lowThreshold)
+    const spans = getSpans(contrastMap)
+    const glitched = sortSpansInImage(imgData, spans)
+    ctx?.putImageData(glitched, 0, 0)
+  }
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setFileDragged(true)
+  }
+
+  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setFileDragged(false)
+  }
+
+
+  const handleDrop = async (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    const files = e.dataTransfer?.files
+
+    if (!files || files.length === 0) {
+      return
+    }
+
+    const file = files[0]
+
+    if (file.type.startsWith('image/')) {
+      const reader = new FileReader()
+      reader.onload = function (e) {
+        const image = new Image()
+        image.src = e.target?.result as string
+        image.onload = function () {
+          const canvas = imgRef.current
+          if (!canvas) {
+            return
+          }
+          canvas.width = image.width
+          canvas.height = image.height
+          ctx?.drawImage(image, 0, 0)
+          setImgData(ctx?.getImageData(0, 0, canvas.width, canvas.height) ?? null)
+        }
+      }
+      reader.readAsDataURL(file)
+    }
+    setFileDragged(false)
+  }
+
+
+  return (
+    <>
+      <div>
+        <div
+          className={clsx("image-container", fileDragged ? "highlight" : "")}
+          onDragOver={handleDragOver}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+        >
+          <canvas ref={imgRef} id="image"></canvas>
+        </div>
+        <button onClick={handleGlitch}>Glitch me</button>
+        <div>
+          <label>Low threshold</label>
+          <input type="range" min="0" max="1" step="0.01" value={lowThreshold} onChange={handleLowThresholdChange} id="contrast-low" />
+          {lowThreshold.toFixed(2)}
+          <br />
+          <label>High threshold</label>
+          <input type="range" min="0" max="1" step="0.01" value={highThreshold} onChange={handleHighThresholdChange} id="contrast-high" />
+          {highThreshold.toFixed(2)}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/util.ts
@@ -0,0 +1,119 @@
+
+export type RGBA = [number, number, number, number];
+
+export function getRGBA(imgData: ImageData, x: number, y: number): RGBA {
+    const i = y * imgData.width * 4 + x * 4
+    return [imgData.data[i], imgData.data[i + 1], imgData.data[i + 2], imgData.data[i + 3]]
+}
+
+export function posToIndex(width: number, stride: number = 4): (x: number, y: number) => number {
+    return (x, y) => y * width * stride + x * stride
+}
+
+export function getContrastMap(imgData: ImageData, upperThresh: number, lowerThresh: number) {
+    const contrastMap = new ImageData(imgData.width, imgData.height)
+    for (let i = 0; i < imgData.data.length; i += 4) {
+        const r = imgData.data[i];
+        const g = imgData.data[i + 1];
+        const b = imgData.data[i + 2];
+        const rawBrightness = r * 0.299 + g * 0.587 + b * 0.114
+        // normalized
+        const brightness = rawBrightness / 255
+        if (brightness > upperThresh || brightness < lowerThresh) {
+            contrastMap.data[i] = 0
+            contrastMap.data[i + 1] = 0
+            contrastMap.data[i + 2] = 0
+            contrastMap.data[i + 3] = 255
+        } else {
+            contrastMap.data[i] = 255
+            contrastMap.data[i + 1] = 255
+            contrastMap.data[i + 2] = 255
+            contrastMap.data[i + 3] = 255
+        }
+    }
+    return contrastMap
+}
+
+export function sortByHue(a: RGBA, b: RGBA) {
+    const hueA = getHue(a)
+    const hueB = getHue(b)
+    return hueB - hueA
+}
+
+export function sortByBrightness(a: RGBA, b: RGBA) {
+    const brightnessA = getBrightness(a)
+    const brightnessB = getBrightness(b)
+    return brightnessB - brightnessA
+}
+
+export function getHue(rgba: RGBA) {
+    const r = rgba[0] / 255
+    const g = rgba[1] / 255
+    const b = rgba[2] / 255
+    const max = Math.max(r, g, b)
+    const min = Math.min(r, g, b)
+    const d = max - min
+    let h
+    if (d === 0) {
+        h = 0
+    } else if (max === r) {
+        h = (g - b) / d + (g < b ? 6 : 0)
+    } else if (max === g) {
+        h = (b - r) / d + 2
+    } else {
+        h = (r - g) / d + 4
+    }
+    return h * 60
+}
+
+
+export function getBrightness(rgba: RGBA) {
+    return rgba[0] * 0.299 + rgba[1] * 0.587 + rgba[2] * 0.114
+}
+
+export function equal(a: RGBA, b: RGBA) {
+    return a[0] === b[0] && a[1] === b[1] && a[2] === b[2] && a[3] === b[3]
+}
+
+export function getSpans(imgData: ImageData): number[][][] {
+    const spans = []
+    // Iterate over each row of the image
+    for (let y = 0; y < imgData.height; y++) {
+        // Iterate over each pixel in the row
+        const xspans = []
+        let currentPixel = getRGBA(imgData, 0, y)
+        let i = 0
+
+        for (let x = 0; x < imgData.width; x++) {
+            // Get the RGBA values for the pixel
+            const nextPixel = getRGBA(imgData, x, y)
+            if (!equal(currentPixel, nextPixel)) {
+                xspans.push([i, x])
+                i = x
+                currentPixel = nextPixel
+            }
+        }
+
+        spans.push(xspans)
+    }
+    return spans
+}
+
+export function sortSpansInImage(srcImg: ImageData, spans: number[][][]) {
+    const imgData = new ImageData(srcImg.width, srcImg.height, { colorSpace: srcImg.colorSpace })
+    imgData.data.set([...srcImg.data])
+
+    const index = posToIndex(imgData.width)
+    spans.forEach((xspans, y) => {
+        xspans.forEach(([from, to]) => {
+            const pixelGroup = []
+            for (let x = from; x < to; x++) {
+                pixelGroup.push(getRGBA(imgData, x, y))
+            }
+            pixelGroup.sort(sortByHue)
+            const fi = index(from, y)
+            imgData.data.set(pixelGroup.flat(), fi)
+        })
+    })
+    return imgData
+}
