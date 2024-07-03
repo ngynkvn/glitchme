aa0f1dd684ebd9b2576eb9974acd6a8774f5871b
Kevin <ngynkvn@gmail.com>
2024-07-02 22:08:47 -0400

    fix: publicPath

--- farm.config.ts
+++ farm.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "@farmfe/core";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: ["@farmfe/plugin-react"],
+  compilation: {
+    output: {
+      publicPath: "/glitchme/",
+    },
+  },
 });
